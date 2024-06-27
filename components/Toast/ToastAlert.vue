d781f2f620d32504c9629151bf99fa7a7fcefc87
denysmalaiko <malaiko.denis@gmail.com>
2024-06-27 19:39:00 +0200

    Register | Add Toast

---
+++ components/Toast/ToastAlert.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-snackbar
+    v-model="toast"
+    vertical
+  >
+    {{ message }}
+  </v-snackbar>
+</template>
+
+<script setup>
+import { useToastAlert } from './ToastAlert.ts';
+
+const { toast, message, open } = useToastAlert();
+
+defineExpose({
+  open
+});
+</script>
